f7b18ab2f2cb1105a73ba2165620366d901b1711
Shaunnn_M <shaunnn-m@outlook.com>
2024-09-09 13:52:08 +0200

    undocumented feature(bug) fixed.

--- TuToR/MainComponents/UpdateData.js
+++ TuToR/MainComponents/UpdateData.js
@@ -1,11 +1,12 @@
-// hooks/useUserDataListener.js
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { doc, onSnapshot } from 'firebase/firestore';
 import { firestoreDB } from '../Config/firebaseConfig'; // Your Firebase setup
 import { setUser } from '../context/actions/userActions'; // Your Redux action
+import _ from 'lodash'; // Use lodash to deep compare objects
 
 const useUserDataListener = () => {
+
     const dispatch = useDispatch();
     const currentUser = useSelector((state) => state.user.user);
 
@@ -17,7 +18,11 @@ const useUserDataListener = () => {
             const unsubscribe = onSnapshot(doc(firestoreDB, 'users', userId), (docSnapshot) => {
                 if (docSnapshot.exists()) {
                     const updatedUserData = docSnapshot.data();
-                    dispatch(setUser(updatedUserData)); // Update Redux store with new data
+
+                    // Compare if the fetched data is different from the current user data in Redux
+                    if (!_.isEqual(updatedUserData, currentUser)) {
+                        dispatch(setUser(updatedUserData)); // Only update Redux store if the data has changed
+                    }
                 } else {
                     console.error('No such document!');
                 }
