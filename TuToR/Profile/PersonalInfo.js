3ec4d0958fd4bd0ead97b725a54740f9a04c1d9a
Shaunnn_M <shaunnn-m@outlook.com>
2024-09-12 00:57:48 +0200

    added extra modifiable fields for the tutor.

--- TuToR/Profile/PersonalInfo.js
+++ TuToR/Profile/PersonalInfo.js
@@ -9,15 +9,18 @@ import {
   ActivityIndicator,
   Dimensions,
   Image,
-    SafeAreaView,
-  Platform, KeyboardAvoidingView, ScrollView
+  SafeAreaView,
+  Platform,
+  KeyboardAvoidingView,
+  ScrollView,
 } from 'react-native';
 import { doc, getDoc, updateDoc } from 'firebase/firestore';
 import { firestoreDB, firebaseStorage } from '../Config/firebaseConfig';
 import { ref, uploadBytes, getDownloadURL } from 'firebase/storage';
 import * as ImagePicker from 'expo-image-picker';
-import {useSelector} from "react-redux";
-import {Ionicons} from "@expo/vector-icons";
+import * as DocumentPicker from 'expo-document-picker';
+import { useSelector } from "react-redux";
+import { Ionicons } from "@expo/vector-icons";
 
 const { width, height } = Dimensions.get('window');
 
@@ -27,9 +30,11 @@ const PersonalInfo = () => {
   const [phoneNumber, setPhoneNumber] = useState('');
   const [gender, setGender] = useState('');
   const [address, setAddress] = useState('');
+  const [cvFileUrl, setCvFileUrl] = useState('');
+  const [transcriptFileUrl, setTranscriptFileUrl] = useState('');
+  const [idFileUrl, setIdFileUrl] = useState('');
   const [loading, setLoading] = useState(false);
   const [loadingProfile, setLoadingProfile] = useState(true);
-  const [profilePicture, setProfilePicture] = useState(null);
   const [profilePictureUrl, setProfilePictureUrl] = useState('');
   const [uploadingImage, setUploadingImage] = useState(false);
   const [isEditing, setIsEditing] = useState(false);
@@ -40,8 +45,11 @@ const PersonalInfo = () => {
     const fetchUserData = async () => {
       try {
         setLoadingProfile(true);
+
+        // Fetch user from 'users' collection
         const userDoc = await getDoc(doc(firestoreDB, 'users', userId));
         const userData = userDoc.data();
+
         if (userData) {
           setName(userData.name || '');
           setLastName(userData.lastName || '');
@@ -49,6 +57,21 @@ const PersonalInfo = () => {
           setGender(userData.gender || '');
           setAddress(userData.address || '');
           setProfilePictureUrl(userData.imageUrl || '');
+
+          // Fetch data from 'Students' or 'Tutors' collection based on role
+          if (currentUser.role === 'Tutor') {
+            const tutorDoc = await getDoc(doc(firestoreDB, 'Tutors', userId));
+            const tutorData = tutorDoc.data();
+            setGender(tutorData?.gender || '');
+            setAddress(tutorData?.address || '');
+            setCvFileUrl(tutorData?.cvFileUrl || '');
+            setTranscriptFileUrl(tutorData?.transcriptFileUrl || '');
+            setIdFileUrl(tutorData?.idFileUrl || '');
+          } else if (currentUser.role === 'Student') {
+            const studentDoc = await getDoc(doc(firestoreDB, 'Students', userId));
+            const studentData = studentDoc.data();
+            // Populate any specific fields for students, if necessary
+          }
         }
       } catch (error) {
         console.error('Error fetching user data: ', error);
@@ -60,6 +83,7 @@ const PersonalInfo = () => {
     fetchUserData();
   }, [userId]);
 
+
   const handleSaveChanges = async () => {
     if (!name || !lastName || !phoneNumber) {
       Alert.alert('Error', 'Please fill in all fields.');
@@ -68,46 +92,51 @@ const PersonalInfo = () => {
 
     try {
       setLoading(true);
-      const userDocRef = doc(firestoreDB, 'users', userId);
 
-      // Update user document in 'users' collection
+      // Update 'users' collection
+      const userDocRef = doc(firestoreDB, 'users', userId);
       await updateDoc(userDocRef, {
-        name: name,
-        lastName: lastName,
-        phoneNumber: phoneNumber,
-        gender: gender,
-        address: address,
+        name,
+        lastName,
+        phoneNumber,
+        gender,
+        address,
         imageUrl: profilePictureUrl,
+        ...(currentUser.role === 'Tutor' && {
+          cvFileUrl,
+          transcriptFileUrl,
+          idFileUrl,
+        }),
       });
 
-      // Conditionally update additional collections (students or tutors)
-      if (currentUser.role === 'student') {
-        const studentDocRef = doc(firestoreDB, 'Students', userId);
-        await updateDoc(studentDocRef, {
-          name: name,
-          lastName: lastName,
-          phoneNumber: phoneNumber,
-          gender: gender,
-          address: address,
-          imageUrl: profilePictureUrl,
-        });
-      }
-
-      if (currentUser.role === 'tutor') {
+      // Update 'Tutors' or 'Students' collection based on role
+      if (currentUser.role === 'Tutor') {
         const tutorDocRef = doc(firestoreDB, 'Tutors', userId);
         await updateDoc(tutorDocRef, {
-          name: name,
-          lastName: lastName,
-          phoneNumber: phoneNumber,
-          gender: gender,
-          address: address,
+          name,
+          lastName,
+          phoneNumber,
+          gender,
+          address,
+          imageUrl: profilePictureUrl,
+          cvFileUrl,
+          transcriptFileUrl,
+          idFileUrl,
+        });
+      } else if (currentUser.role === 'Student') {
+        const studentDocRef = doc(firestoreDB, 'Students', userId);
+        await updateDoc(studentDocRef, {
+          name,
+          lastName,
+          phoneNumber,
+          gender,
+          address,
           imageUrl: profilePictureUrl,
         });
       }
 
-      setIsEditing(false);
       Alert.alert('Success', 'Your information has been updated.');
-
+      setIsEditing(false);
     } catch (error) {
       console.error('Error updating user information: ', error);
       Alert.alert('Error', 'Failed to update information.');
@@ -115,6 +144,7 @@ const PersonalInfo = () => {
       setLoading(false);
     }
   };
+
 
   const handlePickImage = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
@@ -124,10 +154,8 @@ const PersonalInfo = () => {
       quality: 1,
     });
 
-    if (!result.cancelled) {
-      setProfilePicture(result.assets[0].uri);
+    if (!result.canceled) {
       setUploadingImage(true);
-
       try {
         const response = await fetch(result.assets[0].uri);
         const blob = await response.blob();
@@ -135,125 +163,147 @@ const PersonalInfo = () => {
         await uploadBytes(storageRef, blob);
         const url = await getDownloadURL(storageRef);
         setProfilePictureUrl(url);
-        console.log('Image uploaded successfully! URL:', url);
       } catch (error) {
         console.error('Error uploading image:', error.message || error);
-        Alert.alert('Error', 'Failed to upload profile picture. Please try again.');
+        Alert.alert('Error', 'Failed to upload profile picture.');
       } finally {
         setUploadingImage(false);
       }
     }
   };
 
+  const uploadFileToStorage = async (uri, file) => {
+    const blob = await new Promise((resolve, reject) => {
+      const xhr = new XMLHttpRequest();
+      xhr.onload = function () {
+        resolve(xhr.response);
+      };
+      xhr.onerror = function (e) {
+        reject(new TypeError("Network Error"));
+      };
+      xhr.responseType = 'blob';
+      xhr.open("GET", uri, true);
+      xhr.send(null);
+    });
+
+    try {
+      const storageRef = ref(firebaseStorage, `Tutors/${userId}/${file}`);
+      await uploadBytes(storageRef, blob);
+      blob.close();
+      return await getDownloadURL(storageRef);
+    } catch (err) {
+      console.error(err.message);
+      Alert.alert('Error', 'Failed to upload document.');
+    }
+  };
+
+  const pickDocument = async (fileType) => {
+    const result = await DocumentPicker.getDocumentAsync({
+      type: '*/*',
+      copyToCacheDirectory: true,
+    });
+
+    if (!result.canceled) {
+      const url = await uploadFileToStorage(result.assets[0].uri, fileType);
+      if (fileType === 'CV') setCvFileUrl(url);
+      else if (fileType === 'Transcript') setTranscriptFileUrl(url);
+      else if (fileType === 'ID') setIdFileUrl(url);
+    }
+  };
+
   if (loadingProfile) {
     return (
-      <View style={styles.loadingContainer}>
-        <ActivityIndicator size="large" color="#00243a" />
-        <Text>Loading your profile...</Text>
-      </View>
+        <View style={styles.loadingContainer}>
+          <ActivityIndicator size="large" color="#00243a" />
+          <Text>Loading your profile...</Text>
+        </View>
     );
   }
 
   return (
       <SafeAreaView style={styles.safeArea}>
-      <KeyboardAvoidingView
-          style={styles.container}
-          behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-      >
-        <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+        <KeyboardAvoidingView style={styles.container} behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+          <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+            <Text style={styles.headerText}>Update Personal Information</Text>
 
-        <Text style={styles.headerText}>Update Personal Information</Text>
-
-
-        <TouchableOpacity onPress={isEditing ? handlePickImage : null}  style={styles.profilePictureContainer}>
-          {profilePictureUrl ? (
-              <Image source={{uri: currentUser.imageUrl}} style={styles.profilePicture}/>
-          ) : profilePicture ? (
-              <Image source={{uri: currentUser.imageUrl}} style={styles.profilePicture}/>
-          ) : (
-              <Text style={styles.profilePictureText}>Add Profile Picture</Text>
-          )}
-        </TouchableOpacity>
-
-        <TouchableOpacity
-            style={styles.editButton}
-            onPress={() => setIsEditing(!isEditing)}
-        >
-          <Ionicons
-              name={isEditing ? 'close' : 'create'} // Show "create" icon for edit and "close" icon for cancel
-              size={20}
-              color="#000"
-              style={styles.editIcon}
-          />
-          <Text style={styles.editButtonText}>{isEditing ? 'Cancel' : 'Edit'}</Text>
-        </TouchableOpacity>
-
-
-        {uploadingImage && (
-            <ActivityIndicator size="small" color="#00243a" style={styles.uploadingIndicator}/>
-        )}
-
-        <Text style={styles.label}>First Name</Text>
-        <TextInput
-            style={styles.input}
-            placeholder="Enter your first name"
-            value={name}
-            onChangeText={setName}
-            editable={isEditing}
-        />
-
-        <Text style={styles.label}>Last Name</Text>
-        <TextInput
-            style={styles.input}
-            placeholder="Enter your last name"
-            value={lastName}
-            onChangeText={setLastName}
-            editable={isEditing}
-        />
-
-        <Text style={styles.label}>Phone Number</Text>
-        <TextInput
-            style={styles.input}
-            placeholder="Enter your phone number"
-            value={phoneNumber}
-            keyboardType="phone-pad"
-            onChangeText={setPhoneNumber}
-            editable={isEditing}
-        />
-
-        <Text style={styles.label}>Gender</Text>
-        <TextInput
-            style={styles.input}
-            placeholder="Enter your gender"
-            value={gender}
-            onChangeText={setGender}
-            editable={isEditing}
-        />
-
-        <Text style={styles.label}>Address</Text>
-        <TextInput
-            style={styles.input}
-            placeholder="Enter your address"
-            value={address}
-            onChangeText={setAddress}
-            editable={isEditing}
-        />
-
-        {isEditing && ( // Only show save button when editing
-            <TouchableOpacity
-                style={styles.saveButton}
-                onPress={handleSaveChanges}
-                disabled={loading}
-            >
-              {loading ? (
-                  <ActivityIndicator size="small" color="#ffffff" />
+            {/* Profile Picture Section */}
+            <TouchableOpacity onPress={isEditing ? handlePickImage : null} style={styles.profilePictureContainer}>
+              {profilePictureUrl ? (
+                  <Image source={{ uri: profilePictureUrl }} style={styles.profilePicture} />
               ) : (
-                  <Text style={styles.saveButtonText}>Save Changes</Text>
+                  <Text style={styles.profilePictureText}>Add Profile Picture</Text>
               )}
             </TouchableOpacity>
-        )}
-        </ScrollView>
-      </KeyboardAvoidingView>
+
+            <TouchableOpacity
+                style={styles.editButton}
+                onPress={() => setIsEditing(!isEditing)}
+            >
+              <Ionicons
+                  name={isEditing ? 'close' : 'create'} // Show "create" icon for edit and "close" icon for cancel
+                  size={20}
+                  color="#000"
+                  style={styles.editIcon}
+              />
+              <Text style={styles.editButtonText}>{isEditing ? 'Cancel' : 'Edit'}</Text>
+            </TouchableOpacity>
+
+            {uploadingImage && (
+                <ActivityIndicator size="small" color="#00243a" style={styles.uploadingIndicator} />
+            )}
+
+            {/* User Fields */}
+            <Text style={styles.label}>First Name</Text>
+            <TextInput style={styles.input} value={name} onChangeText={setName} editable={isEditing} />
+
+            <Text style={styles.label}>Last Name</Text>
+            <TextInput style={styles.input} value={lastName} onChangeText={setLastName} editable={isEditing} />
+
+            <Text style={styles.label}>Phone Number</Text>
+            <TextInput style={styles.input} value={phoneNumber} onChangeText={setPhoneNumber} editable={isEditing} />
+
+            <Text style={styles.label}>Gender</Text>
+            <TextInput style={styles.input} value={gender} onChangeText={setGender} editable={isEditing} />
+
+            <Text style={styles.label}>Address</Text>
+            <TextInput style={styles.input} value={address} onChangeText={setAddress} editable={isEditing} />
+
+            {/* Tutor-Specific Fields */}
+            {currentUser.role === 'Tutor' && (
+                <View>
+                  {/* CV Upload Button */}
+                  <TouchableOpacity onPress={() => pickDocument('cv')} style={styles.uploadButton}>
+                    <Ionicons name="document-outline" size={20} color="#fff" />
+                    <Text style={styles.buttonText}>
+                      {cvFileUrl ? 'CV Uploaded' : cvFileUrl ? cvFileUrl : 'Upload CV'}
+                    </Text>
+                  </TouchableOpacity>
+
+                  {/* Transcript Upload Button */}
+                  <TouchableOpacity onPress={() => pickDocument('transcript')} style={styles.uploadButton}>
+                    <Ionicons name="document-outline" size={20} color="#fff" />
+                    <Text style={styles.buttonText}>
+                      {transcriptFileUrl ? 'Transcript Uploaded' : transcriptFileUrl ? transcriptFileUrl : 'Upload Transcript'}
+                    </Text>
+                  </TouchableOpacity>
+
+                  {/* ID Upload Button */}
+                  <TouchableOpacity onPress={() => pickDocument('id')} style={styles.uploadButton}>
+                    <Ionicons name="document-outline" size={20} color="#fff" />
+                    <Text style={styles.buttonText}>
+                      {idFileUrl ? 'ID Uploaded' : idFileUrl ? idFileUrl: 'Upload ID'}
+                    </Text>
+                  </TouchableOpacity>
+                </View>
+            )}
+
+            {isEditing && (
+                <TouchableOpacity style={styles.saveButton} onPress={handleSaveChanges} disabled={loading}>
+                  {loading ? <ActivityIndicator size="small" color="#ffffff" /> : <Text style={styles.saveButtonText}>Save Changes</Text>}
+                </TouchableOpacity>
+            )}
+          </ScrollView>
+        </KeyboardAvoidingView>
       </SafeAreaView>
   );
 };
@@ -274,6 +324,21 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     marginBottom: height * 0.02,
   },
+  profilePictureContainer: {
+    alignItems: 'center',
+    marginVertical: height * 0.02,
+  },
+  profilePicture: {
+    width: width * 0.3,
+    height: width * 0.3,
+    borderRadius: width * 0.15,
+    borderColor: '#cccccc',
+    borderWidth: 1,
+  },
+  profilePictureText: {
+    fontSize: width * 0.05,
+    color: '#cccccc',
+  },
   editButton: {
     backgroundColor: '#cccccc',
     padding: 10,
@@ -282,11 +347,11 @@ const styles = StyleSheet.create({
     marginVertical: 10,
     width: '40%',
     alignSelf: 'center',
-    flexDirection: 'row', // Add this to display icon and text side by side
-    justifyContent: 'center', // Center the icon and text
+    flexDirection: 'row',
+    justifyContent: 'center',
   },
   editIcon: {
-    marginRight: 8, // Add some space between the icon and text
+    marginRight: 8,
   },
   editButtonText: {
     fontSize: width * 0.04,
@@ -296,7 +361,7 @@ const styles = StyleSheet.create({
     fontSize: width * 0.04,
     color: '#333333',
     marginVertical: 5,
-    left:15,
+    left: 15,
   },
   input: {
     backgroundColor: '#f0f0f0',
@@ -306,6 +371,11 @@ const styles = StyleSheet.create({
     marginVertical: 10,
     fontSize: width * 0.04,
     width: '100%',
+  },
+  docPickerText: {
+    color: '#007BFF',
+    marginVertical: 10,
+    textAlign: 'center',
   },
   saveButton: {
     backgroundColor: '#00243a',
@@ -324,23 +394,20 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
-  profilePictureContainer: {
+  uploadButton: {
+    flexDirection: 'row', // Display icon and text side by side
     alignItems: 'center',
-    marginVertical: height * 0.02,
+    backgroundColor: '#464d53', // Blue background for buttons
+    padding: 12,
+    borderRadius: 15,
+    marginVertical: 10,
+    justifyContent: 'center',
   },
-  profilePicture: {
-    width: width * 0.3,
-    height: width * 0.3,
-    borderRadius: width * 0.15,
-    borderColor: '#cccccc',
-    borderWidth: 1,
-  },
-  profilePictureText: {
-    fontSize: width * 0.05,
-    color: '#cccccc',
-  },
-  uploadingIndicator: {
-    marginTop: 10,
+  buttonText: {
+    color: '#fff', // White text for buttons
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginLeft: 10, // Space between the icon and text
   },
 });
 
